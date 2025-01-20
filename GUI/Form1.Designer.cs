d82e158f7360d43e09c150d305683a67ccadb5ba
TNekozawa <tnekozawa0@gmail.com>
2025-01-21 01:23:51 +0900

    GUIプロジェクトに実処理を追加

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using Core;
 
 namespace GUI
 {
@@ -30,12 +31,88 @@ namespace GUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(52, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(187, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(52, 181);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(187, 23);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Binary Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.RunCalculator);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(297, 51);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(187, 153);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ClearTextBox);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(537, 251);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private Button button2;
     }
 }
--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -1,12 +1,60 @@
+using Core;
+using System.Collections.Generic;
+using System;
 using System.Windows.Forms;
 
 namespace GUI
 {
     public partial class Form1 : Form
     {
+        private Dictionary<int, TextBox>? TextBoxDic;
+
         public Form1()
         {
             InitializeComponent();
+            CreateTextBox();
+            InitializeTextBox();
+        }
+        private void CreateTextBox()
+        {
+            TextBoxDic = new Dictionary<int, TextBox>
+            {
+                { 1, textBox1 },
+                { 2, textBox2 },
+                { 3, textBox3 }
+            };
+        }
+
+        private void InitializeTextBox()
+        {
+            if (TextBoxDic == null)
+            {
+                return;
+            }
+            else
+            {
+                foreach (var box in TextBoxDic.Values)
+                {
+                    box.Text = "";
+                }
+                richTextBox1.Text = "";
+            }
+        }
+
+        private void ClearTextBox(object sender, EventArgs e)
+        {
+            InitializeTextBox();
+        }
+
+        private void RunCalculator(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            string input1 = textBox1.Text;
+            string input2 = textBox2.Text;
+
+            Tuple<string, string> tuple = Processor.RunCalculator(input1, input2);
+            textBox3.Text = tuple.Item1;
+            richTextBox1.Text = tuple.Item2;
         }
     }
 }
